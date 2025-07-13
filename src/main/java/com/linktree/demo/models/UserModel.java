06cb0177edee4eab393276bb263a8b39447efa99
ManavMuthanna <muthannamanav2003@gmail.com>
2025-07-13 17:14:14 +0530

    feat: added models & ORM

---
+++ src/main/java/com/linktree/demo/models/UserModel.java
@@ -0,0 +1,48 @@
+package com.linktree.demo.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+        name = "UUID",
+        strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    private String name;
+
+    private String email;
+
+    // Getters and setters
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/linktree/demo/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.linktree.demo.repositories;
+
+import com.linktree.demo.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+}
